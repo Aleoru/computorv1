864fd68311cdffc3a5276c396740688390a355dc
Aleoru <aleoru0022@gmail.com>
2025-03-26 11:51:55 +0000

    Making monomonial parse

--- computor.py
+++ computor.py
@@ -1,15 +1,27 @@
 import sys
 import re
 
-def parse(exp):
-    regex = [r"[+-]? ?", #sign
-             r"\d"]
-    res = re.find(exp, bar)
-    return(res)
+def parse(expr:str):
+	regexs = [r" ?[+|-]? ?", 	#sign
+			 r"\d+(\.\d+)?",	#coefficient
+			 r" \* X\^",		#literal
+			 r"\d+"]			#exponent
+	
+	mono = dict()
+	l_expr:int = len(expr)
+	key = 0
+	value = 0
+	end:int = 0
+
+	for regex in regexs:
+		span = re.match(regex, expr)
+		end = span.span()[1]
+
+	return(mono)
 
 def computor():
-    print("Polynom: ", sys.argv[1])
-    print("Res: ", parse(sys.argv[1]))
+	print("Polynom: ", sys.argv[1])
+	print("Res: ", parse(sys.argv[1]))
 
 if __name__ =="__main__":
-    computor()
+	computor()
