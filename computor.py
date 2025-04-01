9a57d074589f6c43baf2e981baf75db5157a1d6f
Alejandra del mar Oropeza ruiz <aoropeza@c3r3s4.42malaga.com>
2025-04-01 20:19:06 +0200

    check_expr() and updated parse()

--- computor.py
+++ computor.py
@@ -1,27 +1,59 @@
 import sys
 import re
 
+def check_expr(expr:str):
+	regex = r" ?[+|-]? ?\d+(\.\d+)? \* X\^\d+ ?"
+
+	monos = list()
+
+	while len(expr) > 0:
+		span = re.match(regex, expr)
+		if span is None:
+			return (False)
+		pos = span.span()[1]
+		monos.append(expr[0:pos])
+		expr = expr[pos:len(expr)]
+
+	return monos
+
 def parse(expr:str):
 	regexs = [r" ?[+|-]? ?", 	#sign
 			 r"\d+(\.\d+)?",	#coefficient
 			 r" \* X\^",		#literal
 			 r"\d+"]			#exponent
 	
-	mono = dict()
-	l_expr:int = len(expr)
-	key = 0
-	value = 0
-	end:int = 0
+	res = list()
 
 	for regex in regexs:
 		span = re.match(regex, expr)
-		end = span.span()[1]
+		pos = span.span()[1]
+		if len(res) % 2:
+			if len(res) == 1:
+				res.append(float(res[0] + expr[0:pos]))
+			else:
+				res.append(int(expr[0:pos]))
+		else:
+			res.append(expr[0:pos])
+		expr = expr[pos:len(expr)]
+
+	mono = {res[3] : res[1]}
 
 	return(mono)
 
 def computor():
+	exprs = sys.argv[1].split("=")
+	if len(exprs) == 2:
+		monos_f = check_expr(exprs[0])
+		monos_s = check_expr(exprs[1])
+	else:
+		print("Invalid expression")
+		return 1
 	print("Polynom: ", sys.argv[1])
-	print("Res: ", parse(sys.argv[1]))
+	if check_expr(exprs[0]) and check_expr(exprs[1]):
+		print("Res: ", parse(monos_f[0]))
+	else:
+		print("Wrong expression")
+		return 1
 
 if __name__ =="__main__":
 	computor()
